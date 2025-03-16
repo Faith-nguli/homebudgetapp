a0a746d2040446d69ba5d6da5b24e7d9d2ab7682
cutie-sudo <faith.nguli@student.moringaschool.com>
2025-03-16 17:23:16 +0300

    done

--- backend/views/auth.py
+++ backend/views/auth.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from flask import Blueprint, request, jsonify
 from werkzeug.security import check_password_hash, generate_password_hash
-from flask_jwt_extended import create_access_token, jwt_required
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 from flask_mail import Message
 from models import User, db, TokenBlocklist
 
